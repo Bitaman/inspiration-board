0fdbb8eceb5ba20cb07c467c9f5b43dcc4189faf
Bitaman <b.amani.s@gmail.com>
2019-06-22 21:40:44 -0700

    Updates

---
+++ precache-manifest.11cf01bb387ff181b39a69499731e03d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b42325af1d1d92c2d8debf6d00a3659",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "12102453b869a0f3e68c",
+    "url": "/inspiration-board/static/css/main.0b570879.chunk.css"
+  },
+  {
+    "revision": "abb9cfaa6ed0dcf999f1",
+    "url": "/inspiration-board/static/js/2.e6f15d39.chunk.js"
+  },
+  {
+    "revision": "12102453b869a0f3e68c",
+    "url": "/inspiration-board/static/js/main.bec202f1.chunk.js"
+  },
+  {
+    "revision": "ff446d0d19ecaeb9679b",
+    "url": "/inspiration-board/static/js/runtime~main.ea10e4b3.js"
+  }
+]);
